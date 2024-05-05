ef9fdacd3d3f07c258606f902d9f220302f617e2
m0cki <148272869+m0cki@users.noreply.github.com>
2024-05-05 11:17:42 +0200

    programm works as intended
    i added new methods for the conversion from char to decimal and from decimal to binary + i put it in a while loop, so that it runs again if the user input is not equivalent to 'nein' (german for no)

--- hashcode.java
+++ hashcode.java
@@ -1,46 +1,83 @@
 package main;
 
-import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class hashcode {
 
 	public static void main(String[] args) {
+		boolean run = true;
+		while(run) {
 		System.out.println("Hashcode-Berechnung");
 		System.out.println("Eingabe eines Strings: ");
 		
 		Scanner zeile = new Scanner(System.in);
 		String eingabe = zeile.next();
 		
+	//Array mit User-Input wird erstellt
 		char[] zeichenkette = new char[eingabe.length()];
 		for (int i = 0; i < eingabe.length(); i++) {
 			zeichenkette[i] = eingabe.charAt(i);
         }
 		
+	//Ausgabe der Zeichen des Array
 		System.out.println("---Zeichen---");
 		for (char z : zeichenkette) {
             System.out.println("'" + z + "'");
         }
 		
-		//Umwandlung von Zeichen zu Dezimalzahl
+	//Umwandlung Array in Dezimal-Array
+		deziArray(zeichenkette);
+		int[] deziZeichenkette = deziArray(zeichenkette);
+	//Ausgabe Dezimal-Zeichenkette
 		System.out.println("---Dezimal---");
-		for (char z : zeichenkette) {			
-			String s = Character.toString(z);
-			try {
-				System.out.println(s.getBytes()[0]);
-			} catch (Exception e) {
-				System.out.println("Fehler bei der Eingabe");
-				e.printStackTrace();
-			}
+		for (int d : deziZeichenkette) {			
+			String s = Integer.toString(d);
+			System.out.println(s);
         }
 		
+	//Umwandlung von Dezimal zu Binär 
+		binArray(deziZeichenkette);
+		String[] binZeichenkette = binArray(deziZeichenkette);
+	//Ausgabe Binär-Zeichenkette
 		System.out.println("---Binär---");
-		//Umwandlung von Dezimalzahl zu Binärzahl
-		for(char b : zeichenkette) {
-			String binaer = Integer.toBinaryString(b);
-			System.out.println(binaer);
-		}
+		for (String b : binZeichenkette) {
+			System.out.println(b);
+        }
 		
+		//vereinfachte Hashcode-Berechnung der Eingabe
+		System.out.println("---Hashcode als Zahl---");
+		int hashcode = 0;
+		for (int i = 0; i < eingabe.length(); i++) {
+			hashcode ^= eingabe.charAt(eingabe.length() - 1 - i) << i; 
+		}
+		System.out.println(hashcode);
+		
+		System.out.println("Noch ein Hashcode berechnen? (ja/nein)");
+		Scanner abfrage = new Scanner(System.in);
+		String wiederholung = abfrage.next();
+		
+		if(wiederholung.equals("nein")) {
+			run = false;
+			}
+		}
+	}
+//Methoden
+	//Umwandlung Zeichenkette in Dezimal-Zeichenkette
+	private static int[] deziArray(char[] zeichenkette) {
+		int[] dezimal = new int[zeichenkette.length];
+		for (int i = 0; i < zeichenkette.length; i++) {
+	        dezimal[i] = Character.valueOf(zeichenkette[i]);
+	    }
+		return dezimal;
 	}
 
+	//Umwandlung Dezimal-Zeichenkette in Binär-Zeichenkette 
+	private static String[] binArray(int[] deziZeichenkette) {
+		String[] binZeichenkette = new String[deziZeichenkette.length];
+		for (int i = 0; i < deziZeichenkette.length; i++) {
+	        String binaer = Integer.toBinaryString(deziZeichenkette[i]);
+	        binZeichenkette[i] = binaer;
+	    }
+		return binZeichenkette;
+		}
 }
