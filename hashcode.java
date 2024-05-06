87041309247c6ecf7e219b553eec6101e385766e
m0cki <148272869+m0cki@users.noreply.github.com>
2024-05-06 17:50:17 +0200

    programm is finished and works fine

--- hashcode.java
+++ hashcode.java
@@ -7,92 +7,75 @@ public class hashcode {
 	public static void main(String[] args) {
 		boolean run = true;
 		while(run) {
-		System.out.println("Hashcode-Berechnung");
-		System.out.println("Eingabe eines Strings: ");
-		
-		Scanner zeile = new Scanner(System.in);
-		String eingabe = zeile.next();
-		
-	//Array mit User-Input wird erstellt
-		char[] zeichenkette = new char[eingabe.length()];
-		for (int i = 0; i < eingabe.length(); i++) {
-			zeichenkette[i] = eingabe.charAt(i);
-        }
-		
-	//Ausgabe der Zeichen des Array
-		System.out.println("---Zeichen---");
-		for (char z : zeichenkette) {
-            System.out.println("'" + z + "'");
-        }
-		
-	//Umwandlung Array in Dezimal-Array
-		deziArray(zeichenkette);
-		int[] deziZeichenkette = deziArray(zeichenkette);
-	//Ausgabe Dezimal-Zeichenkette
-		System.out.println("---Dezimal---");
-		for (int d : deziZeichenkette) {			
-			String s = Integer.toString(d);
-			System.out.println(s);
-        }
-		
-	//Umwandlung von Dezimal zu Binär 
-		binArray(deziZeichenkette);
-		String[] binZeichenkette = binArray(deziZeichenkette);
-	//Ausgabe Binär-Zeichenkette
-		System.out.println("---Binär---");
-		for (String b : binZeichenkette) {
-			System.out.println(b);
-        }
-		
-		//vereinfachte Hashcode-Berechnung der Eingabe
-		System.out.println("---Hashcode als Zahl---");
-		int hashcode = 0;
-		for (int i = 0; i < eingabe.length(); i++) {
-			hashcode ^= eingabe.charAt(eingabe.length() - 1 - i) << i; 
-		}
-		System.out.println(hashcode);
-		
+			System.out.println("Hashcode-Berechnung");
+			System.out.println("Eingabe eines Strings: ");
+			
+			Scanner zeile = new Scanner(System.in);
+			String eingabe = zeile.next();
+			
+		//Array mit User-Input wird erstellt
+			char[] zeichenkette = new char[eingabe.length()];
+			for (int i = 0; i < eingabe.length(); i++) {
+				zeichenkette[i] = eingabe.charAt(i);
+	        }
+			
+		//Ausgabe der Zeichen des Array
+			System.out.println("---Zeichen---");
+			for (char z : zeichenkette) {
+	            System.out.println("'" + z + "'");
+	        }
+			
+		//Umwandlung Array in Dezimal-Array
+			deziArray(zeichenkette);
+			int[] deziZeichenkette = deziArray(zeichenkette);
+		//Ausgabe Dezimal-Zeichenkette
+			System.out.println("---Dezimal---");
+			for (int d : deziZeichenkette) {			
+				String s = Integer.toString(d);
+				System.out.println(s);
+	        }
+			
+		//Umwandlung von Dezimal zu Binär 
+			binArray(deziZeichenkette);
+			String[] binZeichenkette = binArray(deziZeichenkette);
+		//Ausgabe Binär-Zeichenkette
+			System.out.println("---Binär---");
+			for (String b : binZeichenkette) {
+				System.out.println(b);
+	        }
+			
+		//Hashcode-Berechnung der Eingabe
+			System.out.println("---Hashcode als Zahl---");
+			int hashcode = 0;
+			for (int i = 0; i < eingabe.length(); i++) {
+				hashcode ^= eingabe.charAt(eingabe.length() - 1 - i) << i; 
+			}
+			System.out.println(hashcode);
+			
 		//Abfrage, ob erneute Hashcode-berechnung durchgeführt werden soll
-		System.out.println("Noch ein Hashcode berechnen? (ja/nein)");
-		Scanner abfrage = new Scanner(System.in);
-		String wiederholung = abfrage.next();
-		
-		abfrageCheck(wiederholung);
-		/*switch(wiederholung) {
-		case "ja":
-			run = true;
-		case "nein":
-			run = false;			
-		}
-		*/
+			boolean fehlerEingabe = false;
+			while (!fehlerEingabe) {
+				System.out.println("Noch ein Hashcode berechnen? (ja/nein)");
+				Scanner abfrage = new Scanner(System.in);
+				String wiederholung = abfrage.next();
+				try {
+					if (wiederholung.equals("ja")) {
+						fehlerEingabe = true; 
+					}
+					else {
+						if (wiederholung.equals("nein")) {
+							fehlerEingabe = true;
+							run = false;
+						}
+					}
+				}
+				catch (Exception e){
+					fehlerEingabe = false;
+				}
+			}
 		}
 	}
 //Methoden
-	//checkt
-	private static boolean abfrageCheck(String wiederholung) {
-		boolean loop = true;
-		int w;
-		if(wiederholung == "ja") {
-			w = 0;
-		} 
-		else {
-			if(wiederholung == "nein") {
-				w = 1;
-			}
-			else {
-				w = 2;
-			}
-		}
-		while(loop) {
-			switch(w) {
-				case 0: 
-					return true;
-				case 1:
-					return false;
-				case 2: 	
-			}
-		}
-	}
 	//Umwandlung Zeichenkette in Dezimal-Zeichenkette
 	private static int[] deziArray(char[] zeichenkette) {
 		int[] dezimal = new int[zeichenkette.length];
